77a35bd8494ff75407910943bcc1cb1e9ebe9259
Shreya1shree5 <47668807+Shreya1shree5@users.noreply.github.com>
2024-12-12 14:12:41 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,30 @@
+resource "google_container_cluster" "primary" {
+  name     = var.cluster_name
+  location = var.zone
+
+  remove_default_node_pool = true
+  deletion_protection = false
+  initial_node_count       = 1
+
+  network    = var.network
+  subnetwork = var.subnetwork
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+  cluster  = google_container_cluster.primary.name
+  location = google_container_cluster.primary.location
+
+  node_count = 2
+
+  node_config {
+    machine_type = var.machine_type
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+  }
+
+  autoscaling {
+    min_node_count = 1
+    max_node_count = 5
+  }
+}
