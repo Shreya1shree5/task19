1fc81616f1902910f1027690727c87cf79633f9f
Shreya1shree5 <47668807+Shreya1shree5@users.noreply.github.com>
2024-12-19 09:29:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "google_container_node_pool" "primary_nodes" {
 
   node_config {
     machine_type = var.machine_type
-    disk_size_gb = 50
+    disk_size_gb = 10
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform",
     ]
