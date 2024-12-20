82be7b53151c2dbb9cbe07b9b4d0a3ab4dcbb3f4
Shreya1shree5 <47668807+Shreya1shree5@users.noreply.github.com>
2024-12-20 08:28:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "google_container_cluster" "primary" {
   name     = var.cluster_name
-  location = var.region
+  region =   var.region
 
   remove_default_node_pool = true
   deletion_protection = false
